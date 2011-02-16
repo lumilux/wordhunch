79ff31394712fbbe08c33e9358a9dec7b0e8a208
Hans E Hyttinen <hans@hanshyttinen.com>
2011-02-16 03:55:34 -0500

    Added Treebank corpus. Added part of Bwog corpus. Added POS tags.

--- wordhunch.py
+++ wordhunch.py
@@ -7,12 +7,13 @@ import time
 import math
 import nltk
 from nltk.corpus import brown
+from nltk.corpus import treebank
 from nltk.corpus import wordnet
 from nltk.corpus import stopwords
 from nltk.tokenize import *
 
-r = redis.Redis(host='localhost', port=6379, db=0)
-rr = redis.Redis(host='localhost', port=6379, db=1)
+r_msg = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host='localhost', port=6379, db=1)
 
 render = web.template.render('templates/', base='base')
 
@@ -34,13 +35,45 @@ process_form = form.Form(
 
 # 3 might be the best
 window = 4
+punctuation_tags = ['(',')',',','--','.',':']
+tag_dict = {'N': 'n', 'V': 'v', 'ADJ': 'a', 'ADV': 'r'}
 
-def compute_concordance():
-	rr.flushdb()
-	words = brown.words()
-	for w_i in xrange(0, len(words)/1):
-		for nearbyword in words[w_i-window:w_i+window+1]:
-			rr.zincrby(words[w_i].lower(), nearbyword.lower(), 1)
+def simp_pos(tag):
+	if q_tag not in tag_dict:
+		return None
+	else:
+		return tag_dict[q_tag]
+		
+def simp_tag(tag):
+	pos = simp_pos(tag)
+	if not pos:
+		return ''
+	else:
+		return pos
+
+def tagkey(pair):
+	word, tag = pair
+	if isinstance(word, str) and isinstance(tag, str):
+		return simp_tag(tag)+':'+word
+
+def compute_concordance(words):
+	for w_i in xrange(0, len(words)):
+		for nearword,neartag in words[w_i-window:w_i+window+1]:
+			word, tag = words[w_i]
+			r.zincrby(tagkey((word.lower(),tag)), tagkey((nearword.lower(),neartag)), 1)
+
+def preprocess_corpora():
+	brown_words = brown.tagged_words(simplify_tags=True)
+	treebank_words = treebank.tagged_words(simplify_tags=True)
+	
+	#this takes forever.
+	bwog_corpus = nltk.corpus.PlaintextCorpusReader('bwog-corpus-txt', '.*\.txt')
+	bwog_sents = bwog_corpus.sents(bwog_corpus.fileids())
+	bwog_words = []
+	for s_i in xrange(0, len(bwog_sents)/1000):
+		bwog_words.extend(nltk.pos_tag(bwog_sents[s_i]))
+	
+	all_tagged_words = brown_words + treebank_words + bwog_words
 
 def sim(dict_a, pairs_b):
 	dict_b = dict(pairs_b)
@@ -63,6 +96,7 @@ def get_syns(input_text):
 	# remove number words
 	# remove helping verbs
 	# tag input words (install NumPy)
+	# use Jython and CRFTagger to tag Bwog corpus and input
 	# match tagged inputs words to correct tagged synonyms
 	# take average of current word vector and corpus word vector for similarity calculation
 	# normalize word vector; save (hash?) as word_sense
@@ -71,35 +105,44 @@ def get_syns(input_text):
 	nostopwords = []
 	input_text = input_text.decode('utf-8')
 	query = [word.lower() for wordlist in [word_tokenize(sent) for sent in sent_tokenize(input_text)] for word in wordlist]
-	for w in query:
-		if unicode(w) not in stopwords.words() and unicode(w) not in u".,\'’!?":
-			nostopwords.append(w)
+	tagged_query = nltk.pos_tag(query)
+	for word,pos in tagged_query:
+		if unicode(word) not in stopwords.words() and pos not in punctuation_tags:
+			nostopwords.append((word,pos))
 	
 	for q_i in xrange(0, len(nostopwords)):
 		#this might be a bad idea.
-		q_word_vect = {}
-		for nearbyword in nostopwords[q_i-window:q_i+window+1]:
-			nearbyword = nearbyword.lower()
-			if nearbyword not in q_word_vect.keys():
-				q_word_vect[nearbyword] = 1
+		q_pair_vect = {}
+		for nearword,neartag in nostopwords[q_i-window:q_i+window+1]:
+			nearpair_key = tagkey(nearword.lower(), neartag)
+			if nearpair_key not in q_pair_vect:
+				q_pair_vect[nearpair_key] = 1
 			else:
-				q_word_vect[nearbyword] += 1
-			rr.zincrby(nostopwords[q_i].lower(), nearbyword, 1)
+				q_pair_vect[nearpair_key] += 1
+			word, tag = nostopwords[q_i]
+			r.zincrby(tagkey(word.lower(), tag), nearpair_key, 1)
 		
-		q_word = nostopwords[q_i]
+		q_pair = nostopwords[q_i]
+		q_word, q_tag = q_pair
+		q_pair_key = tagkey(q_pair)
+		q_pair_syns_key = q_pair_key+':syns'
+		q_pos = simp_pos(q_tag)
 
-		#this needs to be q_word+':'+q_word_sense+':syns'
-		if True: #not rr.exists(q_word+':syns'):
-			synonyms = [lemma.name for lemmalist in [synset.lemmas for synset in wordnet.synsets(q_word)] for lemma in lemmalist]
-			for s_word in synonyms:
-				#q_word_vect = rr.hgetall(q_word)
-				s_word_vect = rr.zrangebyscore(s_word, max='+inf', min='0', withscores=True)
-				sim_score = sim(q_word_vect, s_word_vect)
+		if not r.exists(q_pair_syns_key):
+			syn_lemmas = [lemma for lemmalist in [synset.lemmas for synset in wordnet.synsets(q_word, q_pos)] for lemma in lemmalist]
+			#syn_lemmas = [syn.lemmas for syn in wordnet.synsets(q_word, q_pos)]
+			for lemma in syn_lemmas:
+				syn_word = lemma.name
+				syn_pos = lemma.synset.pos
+				syn_pair_key = tagkey(syn_word, syn_pos)
+				#q_pair_vect = r.hgetall(q_pair)
+				s_pair_vect = r.zrangebyscore(syn_pair_key, max='+inf', min='0', withscores=True)
+				sim_score = sim(q_pair_vect, s_pair_vect)
 				if sim_score:
-					rr.zadd(q_word+':syns', s_word, sim_score)
-		all_syns.append([q_word, rr.zrevrange(q_word+':syns', start=0, num=5, withscores=True)])
+					r.zadd(q_pair_syns_key, syn_pair_key, sim_score)
+		all_syns.append([q_pair_key, r.zrevrange(q_pair_syns_key, start=0, num=5, withscores=True)])
 		#change this to zincrby
-		rr.hincrby('request_frequency', q_word, 1)
+		r.hincrby('request_frequency', q_pair_key, 1)
 	return all_syns
 
 def get_process_form():
@@ -107,8 +150,8 @@ def get_process_form():
 	return render.process(f)
 
 def get_recent_results():
-	queries = r.sort('recent_queries', start=0, num=10, desc=True)
-	texts = r.sort('recent_queries', start=0, num=10, get='*', desc=True)
+	queries = r_msg.sort('recent_queries', start=0, num=10, desc=True)
+	texts = r_msg.sort('recent_queries', start=0, num=10, get='*', desc=True)
 	qtdict = zip(queries, texts)
 	return render.results(qtdict)
 
@@ -131,8 +174,8 @@ class index:
 		
 		user_data = web.input()
 		key = (int)(time.time()*100) #+"_"+str(web.ctx.ip)
-		r.lpush('recent_queries', key)
-		r.set(key, user_data.input_text)
+		r_msg.lpush('recent_queries', key)
+		r_msg.set(key, user_data.input_text)
 		
 		raise web.seeother('/')
 
@@ -153,5 +196,6 @@ class words:
 		
 
 if __name__ == "__main__":
-	compute_concordance()
+	r.flushdb()
+	preprocess_corpora()
 	app.run()
